e002a76164344126991983736e4ab1f8618a3e8e
Eric-Noblot <eric.noblot@gmail.com>
2024-01-26 14:53:53 +0100

    fesf

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-const year = document.querySelector("#year")
-
-// fetch("http://localhost:3000/year")
-// .then(response => response.json())
-// .then((data) => {
-//     console.log(data)
-//     year.textContent = data.year
-// })
 
 fetch("whatyearisit-backend-roan.vercel.app")
 .then(response => response.json())
 .then((data) => {
-    console.log(data)
-    year.textContent = data.year
+    
+    document.querySelector("#year").textContent = data.year
 })
